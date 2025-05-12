32e6bf002f315dc4614fedc5125d08f3dedcaf55
Densho <denj.rey.c@gmail.com>
2025-05-12 17:52:18 -0400

    Simple Hello World app

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {View, Text} from 'react-native';
 
 const App = () => {
-  return null;
+  return (
+    <View>
+      <Text>Hello World!<Text/>
+    </View>
+  )
 };
 
 export default App;
