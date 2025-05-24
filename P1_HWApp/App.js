5573c84ae531b09f78db5b4652f75ce8824ae4b4
Densho <denj.rey.c@gmail.com>
2025-05-24 09:39:18 -0400

    Borders

--- P1_HWApp/App.js
+++ P1_HWApp/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <View
+        style={{
+          borderWidth: 1,
+          borderTopWidth: 5,
+          borderBottomWidth: 10,
+          borderLeftWidth: 3,
+          borderRightWidth: 20,
+          borderColor: '#15099d',
+          borderRadius: 10,
+          borderTopLeftRadius: 100,
+          borderBottomLeftRadius: 20,
+          borderTopRightRadius: 30,
+          borderBottomRightRadius: 30,
+        }}>
+        <Text>Hello World!!</Text>
+      </View>
     </SafeAreaView>
   );
 };
