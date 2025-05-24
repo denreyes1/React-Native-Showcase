05f2dd0722e341d7ea4b7e64c4986fe278cc631b
Densho <denj.rey.c@gmail.com>
2025-05-24 09:54:43 -0400

    Padding

--- P1_HWApp/App.js
+++ P1_HWApp/App.js
@@ -8,12 +8,19 @@ const App = () => {
         style={{
           backgroundColor: '#ff0000',
           margin: 10,
-          // marginTop: 5,
-          // marginLeft: 100,
-          // marginRight: 100,
-          // marginBottom: 100,
+          marginTop: 5,
+          marginLeft: 100,
+          marginRight: 100,
+          marginBottom: 100,
           marginVertical: 50,
           marginHorizontal: 50,
+          padding: 10,
+          // paddingtop: 100,
+          // paddingLeft: 100,
+          // paddingRight: 10,
+          // paddingBottom: 100,
+          paddingHorizontal: 50,
+          paddingVertical: 50,
         }}>
         <Text>Hello World!!</Text>
       </View>
