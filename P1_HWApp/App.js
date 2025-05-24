2e42d040901d3dfce756b831048c9654ee63db18
Densho <denj.rey.c@gmail.com>
2025-05-24 09:45:43 -0400

    Margins

--- P1_HWApp/App.js
+++ P1_HWApp/App.js
@@ -6,17 +6,20 @@ const App = () => {
     <SafeAreaView>
       <View
         style={{
-          borderWidth: 1,
-          borderTopWidth: 5,
-          borderBottomWidth: 10,
-          borderLeftWidth: 3,
-          borderRightWidth: 20,
-          borderColor: '#15099d',
-          borderRadius: 10,
-          borderTopLeftRadius: 100,
-          borderBottomLeftRadius: 20,
-          borderTopRightRadius: 30,
-          borderBottomRightRadius: 30,
+          backgroundColor: '#ff0000',
+          margin: 10,
+          // marginTop: 5,
+          // marginLeft: 100,
+          // marginRight: 100,
+          // marginBottom: 100,
+          marginVertical: 50,
+          marginHorizontal: 50,
+        }}>
+        <Text>Hello World!!</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: '#ff0000',
         }}>
         <Text>Hello World!!</Text>
       </View>
